53116abd0ebca670cc6a6b94d85b2cd3dd839167
amirali <nasrollahiamirali@gmail.com>
2022-08-30 11:13:32 +0430

    first Commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 # Add App and config site 
-include_once "./core/DB.php";
+include_once "./core/Model.php";
 include_once "./core/config.php";
 include_once "./core/App.php";
 include_once "./core/Controller.php";
