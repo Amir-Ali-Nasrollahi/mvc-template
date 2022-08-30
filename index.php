8637c3f47511e50c2f94829b2429ad6a5592f68e
Amir-Ali-Nasrollahi <84186722+Amir-Ali-Nasrollahi@users.noreply.github.com>
2022-08-30 09:00:33 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+# Add App and config site 
+include_once "./core/DB.php";
+include_once "./core/config.php";
+include_once "./core/App.php";
+include_once "./core/Controller.php";
+new App();
